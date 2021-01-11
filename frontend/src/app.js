71e330a0a8ddd00179958456ca931b70d05f089d
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2021-01-12 02:20:03 +0300

    Build Url Router

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,7 +1,25 @@
 import HomeScreen from './screens/HomeScreen.js';
+import ProductScreen from './screens/ProductScreen.js';
+import { parseRequestUrl } from './utils.js';
+import Error404Screen from './screens/Error404Screen.js';
+
+const routes = {
+  '/': HomeScreen,
+  '/product/:id': ProductScreen,
+};
+
 const router = () => {
+  const request = parseRequestUrl();
+  const parseUrl = (request.resource
+    ? `/${request.resource}`
+    : '/') +
+      (request.id ? '/:id' : '') +
+      (request.verb ? `/${request.verb}` : '');
+
+  const screen = routes[parseUrl] ? routes[parseUrl] : Error404Screen;
   const main = document.querySelector('#main-container');
-  main.innerHTML = HomeScreen.render();
+  main.innerHTML = screen.render();
 };
 
 window.addEventListener('load', router);
+window.addEventListener('hashchange', router);

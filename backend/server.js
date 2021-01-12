f22cdffea287ca82d901b2d0e8e214b777bc9dc5
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2021-01-13 01:09:26 +0300

    Create express server; create route for api/products

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const data = require('./data');
+
+const app = express();
+
+app.get('/products', (req, res) => {
+  res.send(data);
+});
+
+
+app.listen(3000, () => console.log('Server running at port 3000'))

fa152555316ade1f3eff132854c1ece82867e2d4
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2021-02-06 23:50:47 +0300

    Add the rest of star ratings

--- frontend/components/Rating.js
+++ frontend/components/Rating.js
@@ -8,12 +8,48 @@ const Rating = {
       <div class='rating'>
         <span>
           <i class="${
-  props.value >= 1
-    ? 'fa fa-star'
-    : props.value >= 0.5
-      ? 'fa fa-star-half-o'
-      : 'fa fa-star-o'
-}"></i>
+            props.value >= 1
+              ? 'fa fa-star'
+              : props.value >= 0.5
+              ? 'fa fa-star-half-o'
+              : 'fa fa-star-o'
+          }"></i>
+        </span>
+        <span>
+          <i class="${
+            props.value >= 2
+              ? 'fa fa-star'
+              : props.value >= 1.5
+              ? 'fa fa-star-half-o'
+              : 'fa fa-star-o'
+          }"></i>
+        </span>
+        <span>
+          <i class="${
+            props.value >= 3
+              ? 'fa fa-star'
+              : props.value >= 2.5
+              ? 'fa fa-star-half-o'
+              : 'fa fa-star-o'
+          }"></i>
+        </span>
+        <span>
+          <i class="${
+            props.value >= 4
+              ? 'fa fa-star'
+              : props.value >= 3.5
+              ? 'fa fa-star-half-o'
+              : 'fa fa-star-o'
+          }"></i>
+        </span>
+        <span>
+          <i class="${
+            props.value >= 5
+              ? 'fa fa-star'
+              : props.value >= 4.5
+              ? 'fa fa-star-half-o'
+              : 'fa fa-star-o'
+          }"></i>
         </span>
       </div>
     `;

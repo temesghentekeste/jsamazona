a7446ec54ddd785f086525cbac40f0947107ca2b
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2021-02-14 00:42:45 +0300

    Add cart sceeen

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,10 +2,13 @@ import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import parseRequestUrl from './utils';
 import Error404Screen from './screens/Error404Screen';
+import CartScreen from './screens/CartScreen';
 
 const routes = {
   '/': HomeScreen,
   '/product/:id': ProductScreen,
+  '/cart/:id': CartScreen,
+  '/cart': CartScreen
 };
 
 const router = async () => {
